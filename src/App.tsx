8063efccf5b071c545060ec711469db980e6f19c
itsmoali <moali@uni.minerva.edu>
2024-07-16 02:59:43 +0500

    Minor commit

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count what u say boi is {count}
+          count  is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
