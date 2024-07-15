e8cf11eda7c52e18c85e207ff0ddc514a103c0fb
itsmoali <moali@uni.minerva.edu>
2024-07-16 03:09:16 +0500

    Minor commit

--- src/App.tsx
+++ src/App.tsx
@@ -19,14 +19,13 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count  is {count}
+          count  is wehowehrow {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> aejofiefjownd save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
       </p>
     </>
   )
